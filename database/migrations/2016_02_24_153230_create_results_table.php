5f76de9141ea54818fae0e2fb150cfe64ed46ce9
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2016-02-24 15:46:38 +0000

    Initial commit - Added Laravel and migrations

---
+++ database/migrations/2016_02_24_153230_create_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->string('cnpj', 18);
+            $table->longText('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('results');
+    }
+}
