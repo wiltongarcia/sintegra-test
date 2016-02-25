137ed836a9bc2c8654d52a362d2fe8012393964d
Wilton de Oliveira Garcia <wiltonog@gmail.com>
2016-02-25 14:00:40 +0000

    Added the middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'siteresult' => \App\Http\Middleware\SiteResult::class,
+        'apiresult' => \App\Http\Middleware\ApiResult::class,
+        'resultindb' => \App\Http\Middleware\ResultInDb::class,
+        'resultrequest' => \App\Http\Middleware\ResultRequest::class,
+        'resultparse' => \App\Http\Middleware\ResultParse::class,
+        'resultsaveindb' => \App\Http\Middleware\ResultSaveInDb::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
